0408ad4cdcd1891ce31810509c256f6d0af8e149
Stefan Hristov <sihristov@hotmail.com>
2025-03-28 01:16:03 +0200

    add About us page with bing map

--- client/src/components/AboutUs.jsx
+++ client/src/components/AboutUs.jsx
@@ -1,22 +1,32 @@
+import { useEffect } from "react";
+
 export default function AboutUs() {
-	return (
-		<div className="content about">
-			<h3>About me</h3>
-			<div>
-				<p>
-					I'm passionate about technology and programming, a journey that led me to create this task management website.
-					Inspired by my learning at SoftUni, this platform is designed to help people manage their time more effectively.
-					It's an evolving space, constantly improving to meet user needs. My goal is to offer a practical and straightforward
-					tool for everyone who seeks better organization in their daily routine. This website reflects my dedication to making
-					time management easier and more accessible for all.
-				</p>
-				<br />
-				<h1>I hope you will like it and shape it alongside with me!</h1>
-				<br />
-				<h1>Thanks for your time and support!</h1>
-			</div>
-		</div>
-	);
+  useEffect(() => {
+    if (window.loadMapScenario) {
+      window.loadMapScenario();
+    }
+  }, []);
+
+  return (
+    <div className="content about-us">
+      <div className="main-info">
+        <h1>About me</h1>
+        <p>
+          I am really passionate about technology and programming, a journey that led me to create this task management
+          website. Inspired by my learning at SoftUni, this platform is designed to help people manage their time more
+          effectively. It's an evolving space, constantly improving to meet user needs. My goal is to offer a practical
+          and straightforward tool for everyone who seeks better organization in their daily routine. This website
+          reflects my dedication to making time management easier and more accessible for all.
+        </p>
+        <br />
+        <p>I hope you will like it and shape it alongside with me!</p>
+      </div>
+      <div className="location">
+        <h2>Located in Sofia, BULGARIA</h2>
+        <div className="location">
+          <div id="bing-map"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-
