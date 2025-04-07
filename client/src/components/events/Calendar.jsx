e2d15816d9885d58560d9b4a9b687385b6db7dc0
Stefan Hristov <sihristov@hotmail.com>
2025-04-07 22:38:19 +0300

    fix missing tbody error

--- client/src/components/events/Calendar.jsx
+++ client/src/components/events/Calendar.jsx
@@ -10,60 +10,62 @@ export default function Calendar() {
             <div className="contentfront">
               <div className="month">
                 <table>
-                  <tr className="orangeTr">
-                    <th>M</th>
-                    <th>T</th>
-                    <th>W</th>
-                    <th>T</th>
-                    <th>F</th>
-                    <th>S</th>
-                    <th>S</th>
-                  </tr>
-                  <tr className="whiteTr">
-                    <th></th>
-                    <th>1</th>
-                    <th>2</th>
-                    <th>3</th>
-                    <th>4</th>
-                    <th>5</th>
-                    <th>6</th>
-                  </tr>
-                  <tr className="whiteTr">
-                    <th>7</th>
-                    <th>8</th>
-                    <th>9</th>
-                    <th>10</th>
-                    <th>11</th>
-                    <th>12</th>
-                    <th>13</th>
-                  </tr>
-                  <tr className="whiteTr">
-                    <th>14</th>
-                    <th>15</th>
-                    <th>16</th>
-                    <th>17</th>
-                    <th>18</th>
-                    <th>19</th>
-                    <th>20</th>
-                  </tr>
-                  <tr className="whiteTr">
-                    <th>21</th>
-                    <th>22</th>
-                    <th>23</th>
-                    <th>24</th>
-                    <th>25</th>
-                    <th>26</th>
-                    <th>27</th>
-                  </tr>
-                  <tr className="whiteTr">
-                    <th>28</th>
-                    <th>29</th>
-                    <th>30</th>
-                    <th>31</th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                  </tr>
+                  <tbody>
+                    <tr className="orangeTr">
+                      <th>M</th>
+                      <th>T</th>
+                      <th>W</th>
+                      <th>T</th>
+                      <th>F</th>
+                      <th>S</th>
+                      <th>S</th>
+                    </tr>
+                    <tr className="whiteTr">
+                      <th></th>
+                      <th>1</th>
+                      <th>2</th>
+                      <th>3</th>
+                      <th>4</th>
+                      <th>5</th>
+                      <th>6</th>
+                    </tr>
+                    <tr className="whiteTr">
+                      <th>7</th>
+                      <th>8</th>
+                      <th>9</th>
+                      <th>10</th>
+                      <th>11</th>
+                      <th>12</th>
+                      <th>13</th>
+                    </tr>
+                    <tr className="whiteTr">
+                      <th>14</th>
+                      <th>15</th>
+                      <th>16</th>
+                      <th>17</th>
+                      <th>18</th>
+                      <th>19</th>
+                      <th>20</th>
+                    </tr>
+                    <tr className="whiteTr">
+                      <th>21</th>
+                      <th>22</th>
+                      <th>23</th>
+                      <th>24</th>
+                      <th>25</th>
+                      <th>26</th>
+                      <th>27</th>
+                    </tr>
+                    <tr className="whiteTr">
+                      <th>28</th>
+                      <th>29</th>
+                      <th>30</th>
+                      <th>31</th>
+                      <th></th>
+                      <th></th>
+                      <th></th>
+                    </tr>
+                  </tbody>
                 </table>
               </div>
               <div className="date">
