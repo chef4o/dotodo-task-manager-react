5e5970b4d9c054191a1937b9877266c70e1df28d
Stefan Hristov <sihristov@hotmail.com>
2025-04-07 14:43:38 +0300

    fix footer date

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="footer">
-      <p>&#169; 2023 DOTODO Task Manager</p>
+      <p>&#169; 2025 DOTODO Task Manager</p>
     </div>
   );
 }
