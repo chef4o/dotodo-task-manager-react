216f393f6616ffd6264f79b8592c65083d290d57
Stefan Hristov <sihristov@hotmail.com>
2025-04-09 22:36:04 +0300

    enhance vite config for testing js files

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,20 @@
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173 },
   build: { sourcemap: true },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "src"),
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
